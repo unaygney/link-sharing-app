288cd898f8a17a7d2e6755884242cea0eb5dd625
Guney Unay <guneymarketing17@hotmail.com>
2024-02-29 15:28:07 +0300

    update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,17 @@
+import Button from "@/components/Button";
+import { TextField } from "@/components/Input";
 export default function Home() {
-  return <main>test</main>;
+  return (
+    <main className="p-10">
+      <div className="flex flex-col gap-10 max-w-[300px]">
+        <Button title="Test" variant="primary" />
+        <Button title="Test" variant="secondary" />
+        <TextField
+          labelTag="Email Address"
+          inputName="name"
+          placeholder="name"
+        />
+      </div>
+    </main>
+  );
 }
