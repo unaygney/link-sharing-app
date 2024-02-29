c19364cc0396e44492c4b3f9c7a48c36e5bb4fc2
Guney Unay <guneymarketing17@hotmail.com>
2024-02-29 15:28:01 +0300

    created button component

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -10,7 +10,18 @@ export default function Button({
   ...rest
 }) {
   return (
-    <button onClick={onClick} {...rest} className={`min-w-[227px] rounded-lg`}>
+    <button
+      onClick={onClick}
+      {...rest}
+      className={clsx(
+        "min-w-[227px] h-[46px] flex items-center justify-center rounded-lg transition-colors delay-150 heading-s",
+        variant === "primary" &&
+          "bg-purple text-white hover:bg-light-purple disabled:bg-very-light-purple disabled:pointer-events-none",
+        variant === "secondary" &&
+          "bg-white text-purple border border-purple hover:bg-very-light-purple disabled:opacity-50 disabled:pointer-events-none",
+        className
+      )}
+    >
       {title}
     </button>
   );
