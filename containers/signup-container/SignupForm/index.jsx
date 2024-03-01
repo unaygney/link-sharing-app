59456f84d981f4d71836df48e2bb6a0817e50ee2
Guney Unay <guneymarketing17@hotmail.com>
2024-03-01 23:03:46 +0300

    created signup page

---
+++ containers/signup-container/SignupForm/index.jsx
@@ -0,0 +1,103 @@
+"use client";
+import React from "react";
+import Button from "@/components/Button";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import Image from "next/image";
+import { FORM_DATA } from "./constant";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { validationSchemaCreateuser } from "@/utils/validation";
+function SignupForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(validationSchemaCreateuser) });
+  const onSubmit = (data) => console.log(data);
+  console.log({ errors });
+  return (
+    <div className="bg-white md:p-10 rounded-lg md:w-[476px]">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-10">
+          <h1 className="text-2xl leading-[150%] font-bold ">Login</h1>
+          <p className="body-m text-dark-gray ">
+            Add your details below to get back into the app
+          </p>
+        </div>
+        <div className="flex flex-col gap-6 text-dark-gray ">
+          {FORM_DATA.map((item) => (
+            <div key={item.id} className="relative flex flex-col gap-1 ">
+              <label
+                className="body-s"
+                htmlFor={item.name}
+                className={`${
+                  errors[item.name]?.message ? "text-red" : "text-dark-gray"
+                }`}
+              >
+                {item.label}
+              </label>
+              <div className="relative ">
+                <input
+                  {...register(item.name)}
+                  name={item.name}
+                  type={item.type}
+                  placeholder={item.placeholder}
+                  className={`relative w-full h-12 pl-[46px] pr-4 body-m outline-none rounded-lg border ${
+                    errors[item.name]?.message
+                      ? "border-red"
+                      : "border-dark-gray"
+                  } focus:border-purple focus:drop-shadow-[.5px_.5px_5px_rgb(90,60,255)] hover:cursor-pointer`}
+                  autoComplete={
+                    item.type === "password" || item.type === "rePassword"
+                      ? "on"
+                      : "off"
+                  }
+                />
+                {/* Icon */}
+                <div className=" absolute top-1/2 left-4 -translate-y-1/2  ">
+                  <Image
+                    src={item.icon}
+                    alt="icon"
+                    width={16}
+                    height={16}
+                    className="object-cover"
+                  />
+                </div>
+
+                {/* Error Message */}
+                {errors[item.name]?.message && (
+                  <div className="absolute right-4 top-1/2 -translate-y-1/2">
+                    <p className="body-s text-red">
+                      {errors[item.name]?.message}
+                    </p>
+                  </div>
+                )}
+
+                {/* <div className="absolute right-4 top-1/2 -translate-y-1/2">
+                    <p className="body-s text-red">Please check again</p>
+                  </div> */}
+              </div>
+            </div>
+          ))}
+
+          <p className="body-s">Password must contain at least 8 characters</p>
+          <Button
+            className="heading-s"
+            title="Create new account"
+            variant="primary"
+            type="submit"
+          />
+        </div>
+      </form>
+      <div className="text-center mt-10">
+        <p className="body-m">Already have an account?</p>
+        <Link href="/login" className="text-purple body-m">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default SignupForm;
