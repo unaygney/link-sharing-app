7d45f725d0f2135646dd5ac4fd35e3a5f5007e14
Guney Unay <guneymarketing17@hotmail.com>
2024-03-10 21:26:00 +0300

    created database.js

---
+++ config/database.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+let connected = false;
+export default async function connectDB() {
+  mongoose.set("strictQuery", true);
+
+  if (connected) {
+    console.log("Mongodb is already connected..");
+    return;
+  }
+
+  // connect to MongoDb
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    connected = true;
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.log(err);
+  }
+}
