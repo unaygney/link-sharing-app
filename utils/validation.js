4d1926eaf827f3bac0e4e9fe3d8a8dbac51929e1
Guney Unay <guneymarketing17@hotmail.com>
2024-04-16 16:00:40 +0300

    :wrench: Added validationSchemaLoginUser

--- utils/validation.js
+++ utils/validation.js
@@ -14,3 +14,14 @@ export const validationSchemaCreateuser = yup.object({
     .oneOf([yup.ref("password"), null], "Passwords must match")
     .required("Please confirm your password."),
 });
+
+export const validationSchemaLoginUser = yup.object({
+  email: yup
+    .string()
+    .required("Email address is required.")
+    .email("Please enter a valid email address."),
+  password: yup
+    .string()
+    .required("Password is required.")
+    .min(8, "Password must be at least 8 characters."),
+});
