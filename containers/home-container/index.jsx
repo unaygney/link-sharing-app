16e80577ac3d1939fe08bed68de32ff5e2ba48e0
Guney Unay <guneymarketing17@hotmail.com>
2024-04-18 12:34:33 +0300

    test

--- containers/home-container/index.jsx
+++ containers/home-container/index.jsx
@@ -1,13 +1,4 @@
 import React from "react";
 export default function HomeContainer() {
-  const deneme = process.env.NEXT_APP_DENEME;
-  const secret = process.env.NEXT_APP_SECRET_KEY;
-  const mong = process.env.NEXT_APP_MONGODB_URI;
-  return (
-    <div>
-      <p> Slug :{deneme}</p>
-      <p>Secret : {secret}</p>
-      <p>Secret : {mong}</p>
-    </div>
-  );
+  return <div>home page</div>;
 }
