eea1ab505c11876b2edfbf36382ca79c921e3aa8
Guney Unay <guneymarketing17@hotmail.com>
2024-04-19 20:15:38 +0300

    :passport_control: Added middleware.js for authentication

---
+++ middleware.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { isAuthPages, verifyJwtToken } from "./utils/auth";
+export async function middleware(req) {
+  const { url, cookies, nextUrl } = req;
+  const { value: token } = cookies.get("token") ?? { value: null };
+
+  const hasVerifyToken = token && (await verifyJwtToken(token));
+  const isAuthPageRequested = isAuthPages(nextUrl.pathname);
+
+  // dashboard page (root page) is only accessible to authenticated users
+  if (nextUrl.pathname === "/") {
+    if (hasVerifyToken) {
+      const response = NextResponse.next();
+      return response;
+    }
+    const response = NextResponse.redirect(new URL("/login", url));
+    return response;
+  }
+
+  //* login and signnup pages are only accessible to unauthenticated users
+  if (isAuthPageRequested) {
+    if (hasVerifyToken) {
+      const response = NextResponse.redirect(new URL("/", url));
+      return response;
+    }
+    const response = NextResponse.next();
+    return response;
+  }
+}
+
+export const config = {
+  matcher: ["/login", "/signup", "/"],
+};
