290d5fc98ee824a92d69dda9ed68f1be15498369
Guney Unay <guneymarketing17@hotmail.com>
2024-04-19 20:19:05 +0300

    :recycle: Refactor SignupForm

--- containers/signup-container/SignupForm/index.jsx
+++ containers/signup-container/SignupForm/index.jsx
@@ -7,7 +7,11 @@ import Image from "next/image";
 import { FORM_DATA } from "./constant";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { validationSchemaCreateuser } from "@/utils/validation";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 function SignupForm() {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -16,18 +20,58 @@ function SignupForm() {
   } = useForm({ resolver: yupResolver(validationSchemaCreateuser) });
 
   const onSubmit = async (data) => {
-    const response = await fetch("/api/create-user", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const { message } = await response.json();
-    console.log(message);
+    try {
+      const response = await fetch("/api/create-user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const res = await response.json();
+      console.log(response);
+      if (response.status === 200) {
+        await toast.success(res.message, {
+          position: "top-right",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        setTimeout(() => {
+          router.push("/login");
+        }, 1500);
+      } else {
+        await toast.error(res.message, {
+          position: "top-right",
+          autoClose: 1500,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    } catch (error) {
+      await toast.error(error.message, {
+        position: "top-right",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
   };
   return (
     <div className="bg-white md:p-10 rounded-lg md:w-[476px]">
+      <ToastContainer />
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-10">
           <h1 className="text-2xl leading-[150%] font-bold ">Login</h1>
