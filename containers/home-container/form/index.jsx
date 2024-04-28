a4e10434cd2c1641cbd0edb87b1404151bd7073a
Guney Unay <guneymarketing17@hotmail.com>
2024-04-28 17:17:47 +0300

    :children_crossing: User experience on the form has been improved

--- containers/home-container/form/index.jsx
+++ containers/home-container/form/index.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm, Controller, useFieldArray } from "react-hook-form";
 import Button from "@/components/Button";
 import "react-toastify/dist/ReactToastify.css";
@@ -13,11 +13,13 @@ import LinksIcon from "@/public/icon-links.svg";
 import EmptyIconMobile from "@/public/illustration-empty-mobile.svg?url";
 
 export default function Form() {
+  const [mounted, setMounted] = useState(false);
   const router = useRouter();
   const {
     register,
     handleSubmit,
     control,
+    reset,
     formState: { errors },
   } = useForm({
     defaultValues: {
@@ -31,7 +33,29 @@ export default function Form() {
   });
 
   const onSubmit = async (data) => {
-    console.log(data);
+    const { links } = data;
+
+    try {
+      const res = await fetch("/api/add-links", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ links }),
+      });
+
+      const response = await res.json();
+      if (res.status === 200) {
+        toast.success(response.message);
+        router.refresh();
+        reset();
+      } else {
+        toast.error(response.message);
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error("Something went wrong");
+    }
   };
 
   const customStyles = {
@@ -54,6 +78,9 @@ export default function Form() {
     }),
   };
 
+  const urlPattern =
+    /^(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})$/;
+
   const customOptions = ALL_MENU_LIST.map((item) => ({
     value: item.value,
     label: (
@@ -70,101 +97,115 @@ export default function Form() {
     ),
   }));
 
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col gap-6 mt-4"
-    >
-      <ToastContainer />
-      <Button
-        onClick={() => append({ platform: "", url: "" })}
-        type="button"
-        variant="secondary"
-        className="flex-shrink-0"
-        title="+ Add new link"
-      />
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-      {fields.length === 0 ? (
-        <div className="bg-light-gray p-5 flex items-center justify-center rounded-xl">
-          <div className="flex flex-col gap-6 items-center text-center ">
-            <div className="relative w-[124px] h-[80px] md:w-[249px] md:h-[160px]">
-              <Image alt="icon" src={EmptyIconMobile} fill />
-            </div>
-            <h3 className="text-custom-black text-2xl md:text-[32px] font-bold">
-              Let's get you started
-            </h3>
-            <p className="text-dark-gray text-base font-normal">
-              Use the “Add new link” button to get started. Once you have more
-              than one link, you can reorder and edit them.
-            </p>
-          </div>
-        </div>
-      ) : (
-        <div className="flex flex-col gap-4  lg:max-h-[509px] lg:overflow-scroll hide-scrollbar  ">
-          {fields.map((field, index) => (
-            <div
-              key={field.id}
-              className="bg-light-gray rounded-xl p-5 flex flex-col gap-3"
-            >
-              <div className="flex justify-between">
-                <button className="inline-flex items-center gap-2 text-dark-gray">
-                  <DragIcon />
-                  <p className="font-bold text-base">Link #{index + 1}</p>
-                </button>
-                <button
-                  onClick={() => remove(index)}
-                  className="text-base text-dark-gray font-normal"
-                  type="button"
-                >
-                  Remove
-                </button>
+  if (mounted) {
+    return (
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-6 mt-4"
+      >
+        <ToastContainer />
+        <Button
+          onClick={() => append({ platform: "", url: "" })}
+          type="button"
+          variant="secondary"
+          className="flex-shrink-0"
+          title="+ Add new link"
+        />
+
+        {fields.length === 0 ? (
+          <div className="bg-light-gray p-5 flex items-center justify-center rounded-xl">
+            <div className="flex flex-col gap-6 items-center text-center ">
+              <div className="relative w-[124px] h-[80px] md:w-[249px] md:h-[160px]">
+                <Image alt="icon" src={EmptyIconMobile} fill />
               </div>
+              <h3 className="text-custom-black text-2xl md:text-[32px] font-bold">
+                Let's get you started
+              </h3>
+              <p className="text-dark-gray text-base font-normal">
+                Use the “Add new link” button to get started. Once you have more
+                than one link, you can reorder and edit them.
+              </p>
+            </div>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-4  lg:max-h-[509px] lg:overflow-scroll hide-scrollbar  ">
+            {fields.map((field, index) => (
+              <div
+                key={field.id}
+                className="bg-light-gray rounded-xl p-5 flex flex-col gap-3"
+              >
+                <div className="flex justify-between">
+                  <button className="inline-flex items-center gap-2 text-dark-gray">
+                    <DragIcon />
+                    <p className="font-bold text-base">Link #{index + 1}</p>
+                  </button>
+                  <button
+                    onClick={() => remove(index)}
+                    className="text-base text-dark-gray font-normal"
+                    type="button"
+                  >
+                    Remove
+                  </button>
+                </div>
 
-              <Controller
-                name={`links[${index}].platform`}
-                control={control}
-                rules={{ required: true }}
-                render={({ field }) => (
-                  <Select
-                    {...field}
-                    options={customOptions}
-                    classNamePrefix="custom-select"
-                    menuPortalTarget={document.body}
-                    styles={customStyles}
-                    menuPosition="fixed"
-                    value={customOptions.find(
-                      (option) => option.value === field.value
-                    )}
-                    onChange={(val) => field.onChange(val.value)}
-                  />
-                )}
-              />
-              <div className="relative">
-                <input
-                  {...register(`links[${index}].url`, { required: true })}
-                  placeholder="Enter the URL"
-                  className="relative w-full h-12 pl-[46px] pr-4 body-m outline-none rounded-lg border border-gray focus:border-purple focus:drop-shadow-[.5px_.5px_5px_rgb(90,60,255)] hover:cursor-pointer"
+                <Controller
+                  name={`links[${index}].platform`}
+                  control={control}
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      options={customOptions}
+                      classNamePrefix="custom-select"
+                      menuPortalTarget={document.body}
+                      styles={customStyles}
+                      menuPosition="fixed"
+                      value={customOptions.find(
+                        (option) => option.value === field.value
+                      )}
+                      onChange={(val) => field.onChange(val.value)}
+                    />
+                  )}
                 />
-                {errors.links && errors.links[index]?.url && (
-                  <p className="text-red-500">This area is required.</p>
-                )}
-                <div className="absolute top-1/2 left-4 -translate-y-1/2 text-custom-black">
-                  <LinksIcon />
+                <div className="relative">
+                  <input
+                    {...register(`links[${index}].url`, {
+                      required: "URL is required",
+                      pattern: {
+                        value: urlPattern,
+                        message: "Please enter a valid URL",
+                      },
+                    })}
+                    placeholder="Enter the URL"
+                    className="relative w-full h-12 pl-[46px] pr-4 body-m outline-none rounded-lg border border-gray focus:border-purple focus:drop-shadow-[.5px_.5px_5px_rgb(90,60,255)] hover:cursor-pointer"
+                  />
+                  {errors.links && errors.links[index]?.url && (
+                    <p className="text-red text-[9px] absolute right-4 top-1/2 -translate-y-1/2">
+                      {errors.links[index].url.message}
+                    </p>
+                  )}
+                  <div className="absolute top-1/2 left-4 -translate-y-1/2 text-custom-black">
+                    <LinksIcon />
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-      )}
+            ))}
+          </div>
+        )}
 
-      <hr className="w-full text-gray" />
-      <Button
-        type="submit"
-        title="Save"
-        variant="primary"
-        className="xl:w-[91px] xl:ml-auto  flex-shrink-0"
-        disabled={fields.length === 0}
-      />
-    </form>
-  );
+        <hr className="w-full text-gray" />
+        <Button
+          type="submit"
+          title="Save"
+          variant="primary"
+          className="xl:w-[91px] xl:ml-auto  flex-shrink-0"
+          disabled={fields.length === 0}
+        />
+      </form>
+    );
+  }
 }
