5697b8f84ceb9693b19693f884b5dfc3d597c228
Guney Unay <guneymarketing17@hotmail.com>
2024-04-29 20:58:57 +0300

    :bug: fix a minor bug

--- app/(preview)/preview/[id]/page.js
+++ app/(preview)/preview/[id]/page.js
@@ -2,10 +2,15 @@ import PreviewContainer from "@/containers/preview-container";
 import React from "react";
 import User from "@/models/userModel";
 import connectDB from "@/config/database";
+import { redirect } from "next/dist/server/api-utils";
 export default async function Preview({ params }) {
   const userId = params.id;
   await connectDB();
   const user = await User.findOne({ _id: userId }, { password: 0 });
+
+  if (!user) {
+    redirect("/");
+  }
 
   return <PreviewContainer user={user} />;
 }
